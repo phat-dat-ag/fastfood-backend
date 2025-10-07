a5a89b4bc2d3928965cbfc320cf99c9dd8dfc264
phat-dat-ag <67agphatdat@gmail.com>
2025-10-07 11:01:48 +0700

    refactor: remove redundant import lines

--- src/main/java/com/example/fastfoodshop/entity/User.java
+++ src/main/java/com/example/fastfoodshop/entity/User.java
@@ -8,11 +8,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
