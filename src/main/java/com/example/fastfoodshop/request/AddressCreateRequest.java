fd1e603486a04ce8bef8a981f1064bfb0faef4b2
phat-dat-ag <67agphatdat@gmail.com>
2025-10-22 18:03:18 +0700

    feat(address-validation): add size constraints for name and detail fields

--- src/main/java/com/example/fastfoodshop/request/AddressCreateRequest.java
+++ src/main/java/com/example/fastfoodshop/request/AddressCreateRequest.java
@@ -2,13 +2,16 @@ package com.example.fastfoodshop.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class AddressCreateRequest {
     @NotBlank(message = "Tên địa chỉ không được để trống")
+    @Size(min = 3, max = 40, message = "Tên địa chỉ từ 3 đến 40 ký tự")
     private String name;
 
+    @Size(max = 100, message = "Nội dung ghi chú không quá 100 ký tự")
     private String detail;
 
     @NotNull(message = "Vĩ độ không được để trống")
