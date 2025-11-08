67ba7631a95e9fe083bc6e2186f5a4961b3a9fcb
phat-dat-ag <67agphatdat@gmail.com>
2025-11-08 15:45:57 +0700

    feat(dto, response): add answer, question dto; quiz response files for user and staff

---
+++ src/main/java/com/example/fastfoodshop/response/QuizResponse.java
@@ -0,0 +1,34 @@
+package com.example.fastfoodshop.response;
+
+import com.example.fastfoodshop.dto.PromotionDTO;
+import com.example.fastfoodshop.dto.QuestionUserDTO;
+import com.example.fastfoodshop.dto.TopicDifficultyDTO;
+import com.example.fastfoodshop.entity.Question;
+import com.example.fastfoodshop.entity.Quiz;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+@Data
+public class QuizResponse {
+    private Long id;
+    private LocalDateTime startedAt;
+    private LocalDateTime completedAt;
+    private LocalDateTime expiredAt;
+    private TopicDifficultyDTO topicDifficulty;
+    private PromotionDTO promotion;
+    private ArrayList<QuestionUserDTO> questions = new ArrayList<>();
+
+    public QuizResponse(Quiz quiz, ArrayList<Question> questions) {
+        this.id = quiz.getId();
+        this.startedAt = quiz.getStartedAt();
+        this.completedAt = quiz.getCompletedAt();
+        this.expiredAt = quiz.getStartedAt().plusSeconds(quiz.getTopicDifficulty().getDuration());
+        this.topicDifficulty = new TopicDifficultyDTO(quiz.getTopicDifficulty());
+        this.promotion = quiz.getPromotion() != null ? new PromotionDTO(quiz.getPromotion()) : null;
+        for (Question question : questions) {
+            this.questions.add(new QuestionUserDTO(question));
+        }
+    }
+}
