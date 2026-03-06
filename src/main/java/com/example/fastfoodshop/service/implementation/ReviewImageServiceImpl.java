c7734a6cf5d9aa30931d38b61b1d9d47f243a72f
phat-dat-ag <67agphatdat@gmail.com>
2026-03-06 10:33:35 +0700

    refactor: extract ReviewImageService interface and move logic to ReviewImageServiceImpl

---
+++ src/main/java/com/example/fastfoodshop/service/implementation/ReviewImageServiceImpl.java
@@ -0,0 +1,43 @@
+package com.example.fastfoodshop.service.implementation;
+
+import com.example.fastfoodshop.entity.Review;
+import com.example.fastfoodshop.entity.ReviewImage;
+import com.example.fastfoodshop.repository.ReviewImageRepository;
+import com.example.fastfoodshop.service.CloudinaryService;
+import com.example.fastfoodshop.service.ReviewImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewImageServiceImpl implements ReviewImageService {
+    private final CloudinaryService cloudinaryService;
+    private final ReviewImageRepository reviewImageRepository;
+
+    private void handleReviewImage(ReviewImage reviewImage, MultipartFile imageFile) {
+        Map<?, ?> result = cloudinaryService.uploadImage(imageFile, "review");
+
+        reviewImage.setImageUrl((String) result.get("secure_url"));
+        reviewImage.setImagePublicId((String) result.get("public_id"));
+    }
+
+    public ArrayList<ReviewImage> createReviewImages(List<MultipartFile> images, Review review) {
+        ArrayList<ReviewImage> reviewImages = new ArrayList<>();
+
+        if (images == null || images.isEmpty()) return reviewImages;
+
+        for (MultipartFile imageFile : images) {
+            ReviewImage reviewImage = new ReviewImage();
+            reviewImage.setReview(review);
+            handleReviewImage(reviewImage, imageFile);
+            reviewImages.add(reviewImage);
+        }
+
+        return reviewImages;
+    }
+}
