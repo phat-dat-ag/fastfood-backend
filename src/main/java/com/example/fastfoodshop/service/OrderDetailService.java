1e6a910b2d2ffd638e88a013debc0c1bed4fbba9
phat-dat-ag <67agphatdat@gmail.com>
2026-03-06 09:33:29 +0700

    refactor: extract OrderDetailService interface and move logic to OrderDetailServiceImpl

--- src/main/java/com/example/fastfoodshop/service/OrderDetailService.java
+++ src/main/java/com/example/fastfoodshop/service/OrderDetailService.java
@@ -2,27 +2,7 @@ package com.example.fastfoodshop.service;
 
 import com.example.fastfoodshop.dto.CartDTO;
 import com.example.fastfoodshop.entity.Order;
-import com.example.fastfoodshop.entity.OrderDetail;
-import com.example.fastfoodshop.entity.Product;
-import com.example.fastfoodshop.repository.OrderDetailRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
-public class OrderDetailService {
-    private final ProductService productService;
-    private final OrderDetailRepository orderDetailRepository;
-
-    public void createOrderDetail(CartDTO cartDTO, Order order) {
-        Product product = productService.findProductOrThrow(cartDTO.getProduct().getId());
-
-        OrderDetail orderDetail = new OrderDetail();
-        orderDetail.setOrder(order);
-        orderDetail.setProduct(product);
-        orderDetail.setQuantity(cartDTO.getQuantity());
-        orderDetail.setDiscountedPrice(cartDTO.getProduct().getDiscountedPrice());
-
-        orderDetailRepository.save(orderDetail);
-    }
+public interface OrderDetailService {
+    void createOrderDetail(CartDTO cartDTO, Order order);
 }
---
+++ src/main/java/com/example/fastfoodshop/service/implementation/OrderDetailServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.fastfoodshop.service.implementation;
+
+import com.example.fastfoodshop.dto.CartDTO;
+import com.example.fastfoodshop.entity.Order;
+import com.example.fastfoodshop.entity.OrderDetail;
+import com.example.fastfoodshop.entity.Product;
+import com.example.fastfoodshop.repository.OrderDetailRepository;
+import com.example.fastfoodshop.service.OrderDetailService;
+import com.example.fastfoodshop.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderDetailServiceImpl implements OrderDetailService {
+    private final ProductService productService;
+    private final OrderDetailRepository orderDetailRepository;
+
+    public void createOrderDetail(CartDTO cartDTO, Order order) {
+        Product product = productService.findProductOrThrow(cartDTO.getProduct().getId());
+
+        OrderDetail orderDetail = new OrderDetail();
+        orderDetail.setOrder(order);
+        orderDetail.setProduct(product);
+        orderDetail.setQuantity(cartDTO.getQuantity());
+        orderDetail.setDiscountedPrice(cartDTO.getProduct().getDiscountedPrice());
+
+        orderDetailRepository.save(orderDetail);
+    }
+}
