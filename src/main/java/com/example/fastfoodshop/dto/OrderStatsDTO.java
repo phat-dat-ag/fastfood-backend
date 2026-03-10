2824c17c8b98a480a9fb5a7494f5fda9f23e4501
phat-dat-ag <67agphatdat@gmail.com>
2026-03-10 13:48:26 +0700

    refactor(order-stats): convert DTO classes to Java records

--- src/main/java/com/example/fastfoodshop/dto/OrderStatsDTO.java
+++ src/main/java/com/example/fastfoodshop/dto/OrderStatsDTO.java
@@ -1,49 +1,21 @@
 package com.example.fastfoodshop.dto;
 
-import lombok.Data;
-
 import java.math.BigDecimal;
 
-@Data
-public class OrderStatsDTO {
-    private Long pendingOrderAmount;
-    private Long confirmedOrderAmount;
-    private Long deliveringOrderAmount;
-    private Long deliveredOrderAmount;
-    private Long cancelledOrderAmount;
+public record OrderStatsDTO(
+        Long pendingOrderAmount,
+        Long confirmedOrderAmount,
+        Long deliveringOrderAmount,
+        Long deliveredOrderAmount,
+        Long cancelledOrderAmount,
 
-    private Long cashOnDeliveryOrderAmount;
-    private Long bankTransferOrderAmount;
+        Long cashOnDeliveryOrderAmount,
+        Long bankTransferOrderAmount,
 
-    private Long discountedOrderAmount;
+        Long discountedOrderAmount,
 
-    private BigDecimal cashOnDeliveryRevenue;
-    private BigDecimal bankTransferRevenue;
-    private BigDecimal totalRevenue;
-
-    public OrderStatsDTO(
-            Long pendingOrderAmount,
-            Long confirmedOrderAmount,
-            Long deliveringOrderAmount,
-            Long deliveredOrderAmount,
-            Long cancelledOrderAmount,
-            Long cashOnDeliveryOrderAmount,
-            Long bankTransferOrderAmount,
-            Long discountedOrderAmount,
-            BigDecimal cashOnDeliveryRevenue,
-            BigDecimal bankTransferRevenue,
-            BigDecimal totalRevenue
-    ) {
-        this.pendingOrderAmount = pendingOrderAmount;
-        this.confirmedOrderAmount = confirmedOrderAmount;
-        this.deliveringOrderAmount = deliveringOrderAmount;
-        this.deliveredOrderAmount = deliveredOrderAmount;
-        this.cancelledOrderAmount = cancelledOrderAmount;
-        this.cashOnDeliveryOrderAmount = cashOnDeliveryOrderAmount;
-        this.bankTransferOrderAmount = bankTransferOrderAmount;
-        this.discountedOrderAmount = discountedOrderAmount;
-        this.cashOnDeliveryRevenue = cashOnDeliveryRevenue;
-        this.bankTransferRevenue = bankTransferRevenue;
-        this.totalRevenue = totalRevenue;
-    }
+        BigDecimal cashOnDeliveryRevenue,
+        BigDecimal bankTransferRevenue,
+        BigDecimal totalRevenue
+) {
 }
