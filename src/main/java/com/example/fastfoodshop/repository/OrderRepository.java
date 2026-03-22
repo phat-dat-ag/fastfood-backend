8473bded41958f845b329e705b3b4f7f3b8d578d
phat-dat-ag <67agphatdat@gmail.com>
2026-03-22 11:16:21 +0700

    chore(order): remove commented-out and unused code

--- src/main/java/com/example/fastfoodshop/repository/OrderRepository.java
+++ src/main/java/com/example/fastfoodshop/repository/OrderRepository.java
@@ -14,25 +14,12 @@ import java.util.Optional;
 public interface OrderRepository extends JpaRepository<Order, Long> {
     Page<Order> findByDeliveredAtIsNullAndCancelledAtIsNull(Pageable pageable);
 
-//    Optional<Order> findByIdAndDeliveredAtIsNullAndCancelledAtIsNull(Long orderId);
-
     Page<Order> findByUserAndDeliveredAtIsNullAndCancelledAtIsNull(User user, Pageable pageable);
 
     Optional<Order> findByIdAndUserAndDeliveredAtIsNullAndCancelledAtIsNull(Long orderId, User user);
 
     @Query("SELECT o FROM Order o WHERE o.user = :user AND (o.deliveredAt IS NOT NULL OR o.cancelledAt IS NOT NULL)")
     Page<Order> findCompletedOrCancelledOrdersByUser(@Param("user") User user, Pageable pageable);
-
-//    @Query("""
-//                SELECT o
-//                FROM Order o
-//                WHERE o.id = :orderId
-//                  AND o.user = :user
-//                  AND (o.deliveredAt IS NOT NULL OR o.cancelledAt IS NOT NULL)
-//            """)
-//    Optional<Order> findCompletedOrCancelledOrderByIdAndUser(@Param("orderId") Long orderId, @Param("user") User user);
-//
-//    Optional<Order> findByIdAndUserAndDeliveredAtIsNotNull(Long id, User user);
 
     @Query(value = """
             SELECT
