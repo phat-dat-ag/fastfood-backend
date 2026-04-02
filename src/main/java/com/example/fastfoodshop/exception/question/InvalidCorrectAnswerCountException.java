f6cb77e09214788a0a0ae2a0bcfba70a4102bcfb
phat-dat-ag <67agphatdat@gmail.com>
2026-04-02 18:03:18 +0700

    refactor(question): reformat exception message for invalid correct answer count exception

--- src/main/java/com/example/fastfoodshop/exception/question/InvalidCorrectAnswerCountException.java
+++ src/main/java/com/example/fastfoodshop/exception/question/InvalidCorrectAnswerCountException.java
@@ -4,6 +4,6 @@ import com.example.fastfoodshop.exception.base.BusinessException;
 
 public class InvalidCorrectAnswerCountException extends BusinessException {
     public InvalidCorrectAnswerCountException(long correctAnswerCount) {
-        super("INVALID_CORRECT_ANSWER_COUNT", "Số lượng đáp án đúng không hợp lệ" + correctAnswerCount);
+        super("INVALID_CORRECT_ANSWER_COUNT", "Số lượng đáp án đúng không hợp lệ: " + correctAnswerCount);
     }
 }
